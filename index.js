39c501434f026c3562f9802ac479f998e1eff459
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-22 00:56:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0;
+
+  function addBump() {
+    counter++;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps
+  };
+
+}
+
+function createAnimal(animalType) {
+
+  return function (deadlyDevice) {
+    return {
+      animalType,
+      deadlyDevice
+    };
+  }
+
+}
+
+var sharkCreator = createAnimal('Shark');
+
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+
+var sharkWithFrickinCannon = sharkCreator('Cannon');
